c6ff2ff21436ad70f7eb1641e6b79acdb6e8191f
kt1313 <tomaklim@wp.pl>
2020-10-11 18:05:11 +0200

    dodane Zad25 czyta wszystkie pliki i liczy linie, ale zle sumuje

--- src/JavaStartZadania/Zad25_FilesLinesCounter.java
+++ src/JavaStartZadania/Zad25_FilesLinesCounter.java
@@ -8,97 +8,60 @@ import java.lang.reflect.InaccessibleObjectException;
 import java.util.Scanner;
 
 public class Zad25_FilesLinesCounter {
+    static int lines = 0;
     static String directoryPath;
     static String filePath;
     static String[] filesArray;
+    static File file;
     static int linesSum = 0;
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         directoryPath = readFolderPath();
-       // fileOrDirectoryExists(directoryPath);
+        // fileOrDirectoryExists(directoryPath);
         //sprawdz czy istnieje File i wtedy odpal readLines
-        checkDirectoryAndStart(directoryPath);
+        checkDirectoryAndStart(file);
         //a wewnatrz sumuj linie
         System.out.println(linesSum);
     }
 
     private static String readFolderPath() throws InaccessibleObjectException {
+        System.out.println();
         System.out.println("Podaj ścieżkę do folderu z plikami:");
         directoryPath = sc.nextLine();
-       // File file = new File(directoryPath);
-        if (fileOrDirectoryExists(directoryPath))
+        file = new File(directoryPath);
+        if (fileOrDirectoryExists(file))
             return directoryPath;
         else return ("Wrong file path.");
     }
 
-    public static boolean fileOrDirectoryExists(String folderPath) {
-        File file = new File(folderPath);
-        return (file.isFile()||file.isDirectory());
+    public static boolean fileOrDirectoryExists(File file) {
+        return (file.isFile() || file.isDirectory());
     }
 
-
-    public static void checkDirectoryAndStart(String directoryPath) throws IOException {
-        File file = new File(directoryPath);
-        filesArray = file.list();
+    public static void checkDirectoryAndStart(File file ) throws IOException {
         try {
-            for (String f : filesArray
+            for (File f : file.listFiles()
             ) {
-                File f1 = new File(f);
-                System.out.println(file.exists());
-                if(f1.isDirectory()){
-                    System.out.println(f1.isDirectory());
-                    directoryPath = f1.toPath().toString();
-                    checkDirectoryAndStart(directoryPath);
-                }else
-                    openFileAndCountLines(f1);
+                if (f.isDirectory()) {
+                    System.out.println(f.isDirectory());
+                    checkDirectoryAndStart(f);
+                } else
+                    openFileAndCountLines(f);
             }
         } catch (Exception ex) {
         }
-        ;
     }
 
     private static void openFileAndCountLines(File fileName) throws IOException {
-        var fileReader = new FileReader(directoryPath+"/"+fileName);
+        var fileReader = new FileReader(fileName);
         var reader = new BufferedReader(fileReader);
-
-        try (
-                fileReader;
-                reader;
-        ) {
-            String nextLine = null;
-            int lines = 0;
-            while ((nextLine = reader.readLine()) != null) {
-                lines++;
-                linesSum += lines;
-            }
+        try (fileReader; reader;) {
+            while (reader.readLine() != null) {lines++;;
+                System.out.println(linesSum);}
+            linesSum += lines;
             System.out.println("Ilość wierszy w pliku: " + fileName + " to " + lines);
         }
     }
-//    File dir = new File(directoryLocation);
-//if(dir.exists()) {
-//        processDirectory(dir);
-//    } else {
-//        System.out.println("Wskazany folder nie istnieje");
-//    }
-//private int countLinesForFile(File file) throws IOException {
-//    BufferedReader reader = new BufferedReader(new FileReader(file));
-//    int counter = 0;
-//    while (reader.readLine() != null) {
-//        counter++;
-//    }
-//    return counter;
-//}
-//private void processDirectory(File dir) throws IOException {
-//    for (File file : dir.listFiles()) {
-//        if (file.isDirectory()) {
-//            processDirectory(file);
-//        } else {
-//            int lines = countLinesForFile(file);
-//            totalCounter += lines;
-//            System.out.println(file.getAbsolutePath() + " --> " + lines );
-//        }
-//    }
-//}
 }
 
