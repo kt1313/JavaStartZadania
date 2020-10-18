b936136c7ebd0842d8afd38ec28518ca8382b201
kt1313 <tomaklim@wp.pl>
2020-10-19 00:46:45 +0200

    dodane Zad26 gra i trabi, teraz czas wybrac co poytrzebujemy

--- src/JavaStartZadania/Zad26_Lottery.java
+++ src/JavaStartZadania/Zad26_Lottery.java
@@ -16,57 +16,63 @@ public class Zad26_Lottery {
         try (Scanner sc = new Scanner(new File(fileName))) {
             sc.nextLine();
             while (sc.hasNextLine()) {
+                //trpochę koślawo
+                //czyta plik dopoki nie napotka pustej linii
+                //dzieli zczytana linie na czesci i wstawia je odpowiednio do tablicy numerow i megaball
                 String line = sc.nextLine();
                 String[] splittedLine = line.split(",");
-                //ależ koślawo... z tablicy pojedynczo wrzucamy numery do numbersArray zaczynajac od 2go miejsca,
                 int posOfNumbers = 1;
                 System.out.println(splittedLine[posOfNumbers]);
                 numbersAsStringsArray.add(splittedLine[posOfNumbers]);
                 System.out.println(splittedLine[posOfNumbers+1]);
                 megaballAsStringArray.add(splittedLine[posOfNumbers+1]);
             }
+            //zamienia na Integery
             numbersArray = splitAndChangeStringArrayToInteger(numbersAsStringsArray);
             megaballArray = splitAndChangeStringArrayToInteger(megaballAsStringArray);
             //klasa Collections ma metodę frequency, ktora zlicza liczbe wystapien danego elementu
+            //ma tez swap do zamieniania miejscami
         } catch(
                 FileNotFoundException e)
-
         {
             System.err.println("Brak pliku " + fileName);
         }
-        for (String n : numbersAsStringsArray
+        for (Integer n : numbersArray
         ) {
             System.out.print(n + " ");
         }
         System.out.println();
-        for (String n : megaballAsStringArray
+        for (Integer n : megaballArray
         ) {
             System.out.print(n + " ");
         }
     }
-
-
-
     static List<Integer> splitAndChangeStringArrayToInteger(List<String> stringArray) {
-
+        int iterationLimit=5;
+if(stringArray.equals(megaballAsStringArray)){
+iterationLimit=1;
+}
         List<Integer> numbersArray = new ArrayList<>();
         Integer[] singleNumbers;
-        System.out.println("długość " + stringArray + " to " + stringArray.size());
+        int j=1;
+
         for (String s : stringArray
         ) {
-            singleNumbers = new Integer[s.length()];
-            for (int i = 0; i < s.length(); i++) {
+
+            singleNumbers = new Integer[iterationLimit];
+
+            for (int i = 0; i < iterationLimit; i++) {
                 String[] line = s.split(" ");
-                numbersArray.add(Integer.parseInt(line[i]));
+                singleNumbers[i]=(Integer.parseInt(line[i]));
+                System.out.println(singleNumbers[i]);
             }
+            System.out.println("To byl wydruk linii: "+ j);
+            j++;
         }
         return numbersArray;
     }
+
 }
-//    private static String addNumbers(String[] additArray){
-//        String[] splittedNumbers=null;
-//        splittedNumbers=line.split(" ");
-//        Arrays.stream(numbersArray).toArray()=splittedNumbers[1].
-//        return splittedNumbers;
+
 
 
