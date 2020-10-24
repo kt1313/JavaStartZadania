8ffb99e729e065b5d1bf65fb81ed053531675441
kt1313 <tomaklim@wp.pl>
2020-10-24 05:28:38 +0200

    dodane Zad27 etap2 cały

---
+++ src/JavaStartZadania/Zad27_Options.java
@@ -0,0 +1,29 @@
+package JavaStartZadania;
+
+enum Zad27_Options {
+    ADD(0,"dodaj kontakt"), FIND(1,"szukaj kontaktu"),
+    REMOVE(2,"usuń kontakt"), EXIT(3,"wyjście z programu");
+    private int optionNr;
+    private String description;
+
+   private Zad27_Options (int optionNr,String description){
+       this.description=description;
+       this.optionNr=optionNr;
+   }
+
+    public int getOptionNr() {
+        return optionNr;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Zad27_Options changeIntToChoiceString(int optionNr){
+       Zad27_Options[] values=values();
+        for (Zad27_Options opt:values
+             ) {if(opt.getOptionNr()==optionNr) return opt;
+
+        } return null;
+    }
+}
