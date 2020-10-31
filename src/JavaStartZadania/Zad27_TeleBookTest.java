4167a0bd7a9c42aefda7158448371698971249c2
kt1313 <tomaklim@wp.pl>
2020-10-31 04:10:30 +0100

    dodane Zad27 etap3 FileWiter - wszystko działa,  :))))

--- src/JavaStartZadania/Zad27_TeleBookTest.java
+++ src/JavaStartZadania/Zad27_TeleBookTest.java
@@ -7,9 +7,7 @@ public class Zad27_TeleBookTest {
     public static void main(String[] args) throws IOException, NamingException {
         Zad27_TeleBookController teleBookController = new Zad27_TeleBookController();
         Zad27_FileReader.createOrFindFile();
-
         teleBookController.addContactFromFile(Zad27_FileReader.openFileAndSendData());
-
         teleBookController.mainLoop();
     }
 }
